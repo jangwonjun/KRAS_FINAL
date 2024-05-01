85b53a5315b3cf6202b68d4bca18fce1c962abdf
jangwonjun <won2005won@naver.com>
2024-05-01 23:21:50 +0900

    modify

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, redirect, render_template, url_for, session
+from modules.flex import login_system
+from env import FLASK_ENUM, SQL
+
+app = Flask(__name__, static_url_path='/static')
+app.secret_key = FLASK_ENUM.SECRET_KEY
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    next = '로그인이 필요합니다.'
+    if request.method == 'POST': 
+        kras_login_system = login_system() 
+        id = request.form.get('id') 
+        password = request.form.get('password') 
+        print(id, password)  
+
+        auth_inform = kras_login_system.new_login(id, password)
+
+        print("정보")
+        print(auth_inform)
+        if auth_inform :
+            next = '로그인성공'
+            session["username"] = auth_inform[0] #세션에 저장
+            session["userid"] = auth_inform[0][1]
+            print(next)
+            return render_template('index.html', state = next, user = auth_inform[0][0])
+        
+        else:
+            status_code = "password_incorrect"
+            return render_template('login.html',status_code = status_code)
+    
+    return render_template('login.html') 
+
+@app.route('/main')
+def main():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True, port=FLASK_ENUM.PORT)
---
+++ modules/flex.py
@@ -0,0 +1,49 @@
+from env import FLASK_ENUM, SQL
+import pymysql
+from flask import Flask, request, redirect, render_template, url_for, session
+
+class login_system():
+    def __init__(self):
+        pass
+    
+    def new_login(self,id,password):
+        self.id = id 
+        self.password = password
+
+        print(id,self.password)
+
+        conn = pymysql.connect(
+            host=SQL.HOST, port=SQL.PORT, user=SQL.ID, passwd=SQL.PASSWORD, db=SQL.DB_NAME, charset='utf8'
+        )
+
+        cursor = conn.cursor()
+
+        id_sql = "SELECT * FROM kras_login WHERE id = %s"
+        cursor.execute(id_sql,(self.id))
+        id_result = cursor.fetchall()
+
+        print(id_result)
+        
+
+        conn.commit()
+        conn.close()
+
+        data = []
+        
+
+        #DB에 아이디 없을때 -> 예외처리완료
+
+        if id_result :
+            if id_result[0][0] == id:
+                print("pass")
+                if id_result[0][1] == password:
+                    data.append((id_result[0][2],id_result[0][0]))
+                else:
+                    status_code = "password_incorrect"
+                    print("아이디 혹은 비밀번호가 잘못되었습니다.") 
+            else:
+                status_code = "password_incorrect"
+                print("아이디 혹은 비밀번호가 잘못되었습니다.")
+
+        return data
+        
